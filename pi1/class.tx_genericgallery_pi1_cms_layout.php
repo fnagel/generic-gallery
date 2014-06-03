e4d598d2c4c7ae81c51c9f8daf4d56365df1c175
Felix Nagel <info@felixnagel.com>
2014-06-03 16:41:42 +0200

    fix BE preview

--- pi1/class.tx_genericgallery_pi1_cms_layout.php
+++ pi1/class.tx_genericgallery_pi1_cms_layout.php
@@ -34,18 +34,12 @@ class tx_genericgallery_cms_layout extends tslib_pibase {
 	/**
 	 * Returns picture slide string
 	 */
-	function getPictureTitle($params, $pObj) {
-
-		if (strlen($params['row']['title']) > 0) {
-			$params['title'] = $params['row']['title'];
-		} else if ($params['row']['images']) {
-			$helperArray = $this->getDAMImageData($params['row']['uid']);
-			if (is_array($helperArray['rows'])) {
-				$params['title'] = (strlen($helperArray['rows']['title']) > 0) ? $helperArray['rows']['title'] : $helperArray['rows']['file_name'];
+	public function getPictureTitle(&$params, $pObj) {
+		if ($params['table'] === 'tx_generic_gallery_pictures') {
+			if (strlen($params['row']['title']) > 0) {
+				$params['title'] = $params['row']['title'];
 			}
 		}
-
-		return $params;
 	}
 
 
@@ -56,13 +50,21 @@ class tx_genericgallery_cms_layout extends tslib_pibase {
 	 * @param    object $pObj A reference to calling object
 	 * @return    string        Information about pi1 plugin
 	 */
-	function getExtensionSummary($params, &$pObj) {
+	public function getExtensionSummary($params, &$pObj) {
 		$content = '';
+		$filesArray = array(
+			'files' => array(),
+			'rows' => array()
+		);
 
 		if ($params['row']['list_type'] == 'generic_gallery_pi1') {
 
 			// get Extension Manager config
 			$extensionConfiguration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['generic_gallery']);
+
+			if ($extensionConfiguration['enable_cms_layout']) {
+				return '';
+			}
 
 			// add mode info
 			if ($params['row']['tx_generic_gallery_images']) {
@@ -71,94 +73,118 @@ class tx_genericgallery_cms_layout extends tslib_pibase {
 				$content .= $GLOBALS['LANG']->sL('LLL:EXT:generic_gallery/locallang_db.xml:generic_gallery_items');
 			}
 
-			// add image info
-			if ($extensionConfiguration['enable_cms_layout']) {
-				if ($params['row']['tx_generic_gallery_images']) {
-					// get multiple images
-//					$filesArray = tx_dam_db::getReferencedFiles("tt_content", $params['row']['uid'], 'tx_generic_gallery_picture_single','', 'tx_dam.*');
-				} else {
-					$filesArray = array();
-					$filesArray["files"] = array();
-					$filesArray["rows"] = array();
+			$content .= '<br /><br />';
 
-					$select = 'uid, title';
-					$table = 'tx_generic_gallery_pictures';
-					$where = 'tt_content_id = ' . $params['row']['uid'];
-					// always (!) use TYPO3 default function for adding hidden = 0, deleted = 0, group and date statements
-					$where .= t3lib_BEfunc::BEenableFields($table);
-					$order = 'sorting';
-					$group = '';
-					$limit = '';
-					$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery($select, $table, $where, $group, $order, $limit);
+			if ($params['row']['tx_generic_gallery_images']) {
+				$content .= $this->thumbCode(
+					$params['row'],
+					'tt_content',
+					'tx_generic_gallery_images',
+					'tx_generic_gallery_picture_single',
+					$GLOBALS['BACK_PATH']
+				);
+			} else {
+				$select = 'uid, title';
+				$table = 'tx_generic_gallery_pictures';
+				$where = 'tt_content_id = ' . $params['row']['uid'];
+				$where .= t3lib_BEfunc::BEenableFields($table);
+				$order = 'sorting';
+				$group = '';
+				$limit = '';
+				$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery($select, $table, $where, $group, $order, $limit);
 
-					while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
-						if (is_array($row)) {
-							$helperArray = $this->getDAMImageData($row['uid']);
-							if (is_array($helperArray)) {
-								$uid = $helperArray["rows"]['uid'];
-								$filesArray["files"][$uid] = $helperArray["files"];
-								$filesArray["rows"][$uid] = $helperArray["rows"];
-								// add item title if set
-								if (strlen($row['title']) > 0) {
-									$filesArray["rows"][$uid]['generic_gallery_title'] = $row['title'];
-								}
-							}
-						}
+				while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
+					if (is_array($row)) {
+						$content .= $this->thumbCode(
+							$row,
+							'tx_generic_gallery_pictures',
+							'images',
+							'tx_generic_gallery_picture_single',
+							$GLOBALS['BACK_PATH']
+						);
 					}
-
-					$GLOBALS['TYPO3_DB']->sql_free_result($res);
 				}
-				// Wrap span-tags:
-				$content .= '<br /><br />' . $this->renderSummery($filesArray);
+
+				$GLOBALS['TYPO3_DB']->sql_free_result($res);
 			}
 		}
 
 		return $content;
 	}
 
-	function getDAMImageData($imgUid) {
-		$damArray = array();
-		$damArray["files"] = array();
-		$damArray["rows"] = array();
+	/**
+	 * Returns a linked image-tag for thumbnail(s)/fileicons/truetype-font-previews from a database row with a list of image files in a field
+	 * All $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'] extension are made to thumbnails + ttf file (renders font-example)
+	 * Thumbsnails are linked to the show_item.php script which will display further details.
+	 *
+	 * @param array $row Row is the database row from the table, $table.
+	 * @param string $table Table name for $row (present in TCA)
+	 * @param string $field Field is pointing to the list of image files
+	 * @param string $fieldname Refrence field FAL
+	 * @param string $backPath Back path prefix for image tag src="" field
+	 * @param int|string $size Optional: $size is [w]x[h] of the thumbnail. 56 is default.
+	 * @param boolean $linkInfoPopup Whether to wrap with a link opening the info popup
+	 *
+	 * @return string Thumbnail image tag.
+	 *
+	 * taken from TYPO3 6.2.3
+	 */
+	protected function thumbCode($row, $table, $field, $fieldname, $backPath, $size = '', $linkInfoPopup = TRUE) {
+		$tcaConfig = $GLOBALS['TCA'][$table]['columns'][$field]['config'];
+		// Check and parse the size parameter
+		$sizeParts = array(64, 64);
+		if ($size = trim($size)) {
+			$sizeParts = explode('x', $size . 'x' . $size);
+			if (!(int)$sizeParts[0]) {
+				$size = '';
+			}
+		}
+		$thumbData = '';
+		// FAL references
+		if ($tcaConfig['type'] === 'inline') {
+			$sortingField = isset($tcaConfig['foreign_sortby']) ? $tcaConfig['foreign_sortby'] : '';
+			$referenceUids = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows(
+				'uid',
+				'sys_file_reference',
+				'tablenames = ' . $GLOBALS['TYPO3_DB']->fullQuoteStr($table, 'sys_file_reference')
+				. ' AND fieldname=' . $GLOBALS['TYPO3_DB']->fullQuoteStr($fieldname, 'sys_file_reference')
+				. ' AND uid_foreign=' . (int)$row['uid']
+				. TYPO3\CMS\Backend\Utility\BackendUtility::deleteClause('sys_file_reference')
+				. TYPO3\CMS\Backend\Utility\BackendUtility::versioningPlaceholderClause('sys_file_reference'),
+				'',
+				$sortingField
+			);
+			foreach ($referenceUids as $referenceUid) {
+				$fileReferenceObject = TYPO3\CMS\Core\Resource\ResourceFactory::getInstance()->getFileReferenceObject($referenceUid['uid']);
+				$fileObject = $fileReferenceObject->getOriginalFile();
 
-//		$damFiles = tx_dam_db::getReferencedFiles('tx_generic_gallery_pictures', intval($imgUid), 'tx_generic_gallery_picture_single','', 'tx_dam.*');
+				if ($fileObject->isMissing()) {
+					$flashMessage = \TYPO3\CMS\Core\Resource\Utility\BackendUtility::getFlashMessageForMissingFile($fileObject);
+					$thumbData .= $flashMessage->render();
+					continue;
+				}
 
-		// check if our row is valid
-		if (isset($damFiles['files']) && count($damFiles['files']) > 0) {
-			$damArray["files"] = current($damFiles['files']);
-			$damArray["rows"] = current($damFiles['rows']);
-
-			return $damArray;
+				// Web image
+				if (TYPO3\CMS\Core\Utility\GeneralUtility::inList($GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'], $fileReferenceObject->getExtension())) {
+					$imageUrl = $fileObject->process(TYPO3\CMS\Core\Resource\ProcessedFile::CONTEXT_IMAGEPREVIEW, array(
+						'width' => $sizeParts[0],
+						'height' => $sizeParts[1]
+					))->getPublicUrl(TRUE);
+					$imgTag = '<img src="' . $imageUrl . '" alt="' . htmlspecialchars($fileReferenceObject->getName()) . '" />';
+				} else {
+					// Icon
+					$imgTag = TYPO3\CMS\Backend\Utility\IconUtility::getSpriteIconForResource($fileObject, array('title' => $fileObject->getName()));
+				}
+				if ($linkInfoPopup) {
+					$onClick = 'top.launchView(\'_FILE\',\'' . $fileObject->getUid() . '\',\'' . $backPath . '\'); return false;';
+					$thumbData .= '<a href="#" onclick="' . htmlspecialchars($onClick) . '">' . $imgTag . '</a> ';
+				} else {
+					$thumbData .= $imgTag;
+				}
+			}
 		}
 
-		return false;
-	}
-
-	function renderSummery($damArray) {
-		$out = "";
-		$count = 0;
-		$shortSum = (count($damArray['rows']) > 10) ? true : false;
-		foreach ($damArray['rows'] as $rowDAM) {
-//			$thumb = tx_dam_guiFunc::thumbnail($rowDAM);
-			$title = "";
-			if ($rowDAM['generic_gallery_title']) {
-				$label = "<strong>" . $GLOBALS['LANG']->sL('LLL:EXT:generic_gallery/locallang_db.xml:generic_gallery_pictures.title') . ":</strong> ";
-				$title = "<p>" . $label . $rowDAM['generic_gallery_title'] . "</p>";
-			}
-			if ($shortSum) {
-				$thumb = '<div style="float:left; width:56px; height:56px; margin: 2px 5px 2px 0; padding: 3px; background-color:#fff; border:solid 1px #ccc;">' . $thumb . '</div>';
-			} else {
-//				$caption = tx_dam_guiFunc::meta_compileInfoData($rowDAM, 'title, file_name, description:truncate:50', 'paragraph');
-				$thumb = '<div style="float:left; width:56px; height:56px; margin: 2px 5px 2px 0; padding: 5px; background-color:#fff; border:solid 1px #ccc;">' . $thumb . '</div>';
-				$thumb = '<div>' . $thumb . $title . $caption . '</div><div style="clear:both"></div>';
-			}
-			$count++;
-
-			$out .= $thumb;
-		}
-		if ($shortSum) $out = '<div>' . $out . '</div><div style="clear:both"></div>';
-
-		return $out;
+		return $thumbData;
 	}
 }
 
