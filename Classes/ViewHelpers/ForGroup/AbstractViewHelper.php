2a5ed488aaba4fe4a3b0c430b7e0675acb2cbe60
Felix Nagel <info@felixnagel.com>
2014-12-05 01:39:48 +0100

    Add ForGroup VH

---
+++ Classes/ViewHelpers/ForGroup/AbstractViewHelper.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace TYPO3\GgExtbase\ViewHelpers\ForGroup;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2014 Felix Nagel <info@felixnagel.com>
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ * ************************************************************* */
+/**
+ * ForGroupViewHelper
+ */
+class AbstractViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
+
+	/**
+	 * Initializes the arguments for the ViewHelper
+	 *
+	 * @return void
+	 */
+	public function initializeArguments() {
+		$this->registerArgument('iteration', 'array', 'The for VH iteration array', TRUE);
+		$this->registerArgument('max', 'int', 'Max items in one for group', TRUE, 2);
+	}
+
+	/**
+	 * Initializes the view helper before invoking the render method.
+	 *
+	 * Override this method to solve tasks before the view helper content is rendered.
+	 *
+	 * @return void
+	 * @api
+	 */
+	public function initialize() {
+		$this->iteration = $this->arguments['iteration'];
+		$this->max = $this->arguments['max'];
+	}
+}
