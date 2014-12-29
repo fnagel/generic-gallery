a6782a266b0aad895dc631dd0b598612e382b021
Felix Nagel <info@felixnagel.com>
2014-12-29 17:13:49 +0100

    Add basic BE preview (migrated from EXT:generic_gallery)

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,6 +3,11 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
+// BE preview
+$TYPO3_CONF_VARS['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info']['ggextbase_gallery'][] =
+	'EXT:gg_extbase/Classes/Backend/Hooks/PageLayoutViewHook.php:TYPO3\GgExtbase\Backend\Hooks\PageLayoutViewHook->getExtensionSummary';
+
+// FE plugin
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
 	'TYPO3.' . $_EXTKEY,
 	'Gallery',
