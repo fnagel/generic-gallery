c6c2242df4f87d6a413acabc8f12bd185c507cd7
Felix Nagel <info@felixnagel.com>
2015-03-14 16:38:07 +0100

    Use core thumbCode for preview rendering

--- Classes/Backend/Hooks/PageLayoutViewHook.php
+++ Classes/Backend/Hooks/PageLayoutViewHook.php
@@ -26,11 +26,7 @@ namespace TYPO3\GenericGallery\Backend\Hooks;
  ***************************************************************/
 
 use \TYPO3\GenericGallery\Utility\EmConfiguration,
-	TYPO3\CMS\Core\Utility\GeneralUtility,
-	TYPO3\CMS\Backend\Utility\BackendUtility,
-	TYPO3\CMS\Core\Resource\ResourceFactory,
-	TYPO3\CMS\Core\Resource\ProcessedFile,
-	TYPO3\CMS\Backend\Utility\IconUtility;
+	TYPO3\CMS\Backend\Utility\BackendUtility;
 
 /**
  * Hook class for PageLayoutView hook `list_type_Info`
@@ -147,7 +143,7 @@ class PageLayoutViewHook {
 	protected function renderImagesPreview() {
 		$this->tableData[] = array('Source', 'images');
 
-		$this->imagePreviewHtml = $this->thumbCode(
+		$this->imagePreviewHtml = BackendUtility::thumbCode(
 			$this->data,
 			'tt_content',
 			'tx_generic_gallery_images',
@@ -187,7 +183,7 @@ class PageLayoutViewHook {
 
 		while (($row = $database->sql_fetch_assoc($res))) {
 			if (is_array($row)) {
-				$result .= $this->thumbCode(
+				$result .= BackendUtility::thumbCode(
 					$row,
 					'tx_generic_gallery_pictures',
 					'images',
@@ -218,89 +214,5 @@ class PageLayoutViewHook {
 		}
 
 		return '<br><pre style="white-space: normal;">' . $content . '</pre>';
-	}
-
-
-	/**
-	 * Returns a linked image-tag for thumbnail(s)/fileicons/truetype-font-previews
-	 * from a database row with a list of image files in a field All
-	 * $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'] extension are made to
-	 * thumbnails + ttf file (renders font-example) Thumbsnails are linked to the
-	 * show_item.php script which will display further details.
-	 *
-	 * @param array $row Row is the database row from the table, $table.
-	 * @param string $table Table name for $row (present in TCA)
-	 * @param string $field Field is pointing to the list of image files
-	 * @param string $fieldname Refrence field FAL
-	 * @param string $backPath Back path prefix for image tag src="" field
-	 * @param int|string $size Optional: $size is [w]x[h] of the thumbnail. 56 is default.
-	 * @param boolean $linkInfoPopup Whether to wrap with a link opening the info popup
-	 *
-	 * @return string Thumbnail image tag.
-	 *
-	 * taken from TYPO3 6.2.3
-	 */
-	protected function thumbCode($row, $table, $field, $fieldname, $backPath, $size = '', $linkInfoPopup = TRUE) {
-		$tcaConfig = $GLOBALS['TCA'][$table]['columns'][$field]['config'];
-		// Check and parse the size parameter
-		$sizeParts = array(64, 64);
-		if (($size = trim($size))) {
-			$sizeParts = explode('x', $size . 'x' . $size);
-			if (!(int)$sizeParts[0]) {
-				$size = '';
-			}
-		}
-		$thumbData = '';
-		// FAL references
-		if ($tcaConfig['type'] === 'inline') {
-			$sortingField = isset($tcaConfig['foreign_sortby']) ? $tcaConfig['foreign_sortby'] : '';
-			$referenceUids = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows(
-				'uid',
-				'sys_file_reference',
-				'tablenames = ' . $GLOBALS['TYPO3_DB']->fullQuoteStr($table, 'sys_file_reference') .
-				' AND fieldname=' . $GLOBALS['TYPO3_DB']->fullQuoteStr($fieldname, 'sys_file_reference') .
-				' AND uid_foreign=' . (int)$row['uid'] .
-				BackendUtility::deleteClause('sys_file_reference') .
-				BackendUtility::versioningPlaceholderClause('sys_file_reference'),
-				'',
-				$sortingField
-			);
-			foreach ($referenceUids as $referenceUid) {
-				$fileReferenceObject = ResourceFactory::getInstance()->getFileReferenceObject($referenceUid['uid']);
-				$fileObject = $fileReferenceObject->getOriginalFile();
-
-				if ($fileObject->isMissing()) {
-					$flashMessage = BackendUtility::getFlashMessageForMissingFile($fileObject);
-					$thumbData .= $flashMessage->render();
-					continue;
-				}
-
-				// Web image
-				if (GeneralUtility::inList(
-					$GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'],
-					$fileReferenceObject->getExtension())
-				) {
-					$imageUrl = $fileObject->process(ProcessedFile::CONTEXT_IMAGEPREVIEW, array(
-						'width' => $sizeParts[0],
-						'height' => $sizeParts[1]
-					))->getPublicUrl(TRUE);
-					$imgTag = '<img src="' . $imageUrl . '" alt="' . htmlspecialchars($fileReferenceObject->getName()) . '" />';
-				} else {
-					// Icon
-					$imgTag = IconUtility::getSpriteIconForResource(
-						$fileObject,
-						array('title' => $fileObject->getName())
-					);
-				}
-				if ($linkInfoPopup) {
-					$onClick = 'top.launchView(\'_FILE\',\'' . $fileObject->getUid() . '\',\'' . $backPath . '\'); return false;';
-					$thumbData .= '<a href="#" onclick="' . htmlspecialchars($onClick) . '">' . $imgTag . '</a> ';
-				} else {
-					$thumbData .= $imgTag;
-				}
-			}
-		}
-
-		return $thumbData;
 	}
 }
