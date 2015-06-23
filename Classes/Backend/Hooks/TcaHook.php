1782591df112b249bba2842ac3929cc6cf902689
Felix Nagel <info@felixnagel.com>
2015-06-23 20:42:15 +0200

    Fix TCA hook for TYPO3 7.x

--- Classes/Backend/Hooks/TcaHook.php
+++ Classes/Backend/Hooks/TcaHook.php
@@ -42,29 +42,27 @@ class TcaHook {
 	 * @param array $config
 	 * @return array The config including the items for the dropdown
 	 */
-	function addPredefinedFields($config) {
-		global $LANG;
-
+	public function addPredefinedFields($config) {
 		if (is_array($config['items'])) {
 			$pid = $config['row']['pid'];
 			if ($pid < 0) {
 				$contentUid = str_replace('-', '', $pid);
-				$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('pid', 'tt_content', 'uid=' . $contentUid);
+				$res = $this->getDatabase()->exec_SELECTquery('pid', 'tt_content', 'uid=' . $contentUid);
 				if ($res) {
-					$row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res);
+					$row = $this->getDatabase()->sql_fetch_assoc($res);
 					$pid = $row['pid'];
-					$GLOBALS['TYPO3_DB']->sql_free_result($res);
+					$this->getDatabase()->sql_free_result($res);
 				}
 			}
 
-			$typoscript = $this->loadTS($pid);
+			$typoscript = $this->loadTypoScript($pid);
 			$settings = $typoscript['plugin.']['tx_genericgallery.']['settings.'];
 			$predef = array();
 
 			// no config available
 			if (!is_array($settings['gallery.']) || sizeof($settings['gallery.']) === 0) {
 				$optionList[] = array(
-					0 => $LANG->sL('LLL:EXT:generic_gallery/Resources/Private/Language/locallang_db.xlf:cms_layout.missing_config'), 1 => ''
+					0 => $this->translate('cms_layout.missing_config'), 1 => ''
 				);
 
 				return $config['items'] = array_merge($config['items'], $optionList);
@@ -83,14 +81,34 @@ class TcaHook {
 			}
 
 			$optionList = array();
-			$optionList[] = array(0 => $LANG->sL('LLL:EXT:generic_gallery/Resources/Private/Language/locallang_db.xlf:cms_layout.please_select'), 1 => '');
+			$optionList[] = array(0 => $this->translate('cms_layout.please_select'), 1 => '');
 			foreach ($predef as $k => $v) {
 				$optionList[] = array(0 => $v, 1 => $k);
 			}
 			$config['items'] = array_merge($config['items'], $optionList);
-
-			return $config;
 		}
+
+		return $config;
+	}
+
+	/**
+	 * @return \TYPO3\CMS\Core\Database\DatabaseConnection
+	 */
+	protected function getDatabase() {
+		return $GLOBALS['TYPO3_DB'];
+	}
+
+	/**
+	 * @param string $key
+	 * @param string $keyPrefix
+	 *
+	 * @return string
+	 */
+	protected function translate(
+		$key,
+		$keyPrefix = 'LLL:EXT:generic_gallery/Resources/Private/Language/locallang_db.xlf'
+	) {
+		return $GLOBALS['LANG']->sL($keyPrefix . ':' . $key);
 	}
 
 	/**
@@ -99,16 +117,18 @@ class TcaHook {
 	 * @param int $pageUid
 	 * @return array The TypoScript setup
 	 */
-	function loadTS($pageUid) {
-		$sysPageObj = GeneralUtility::makeInstance('t3lib_pageSelect');
-		$rootLine = $sysPageObj->getRootLine($pageUid);
+	protected function loadTypoScript($pageUid) {
+		/* @var $sysPageObject \TYPO3\CMS\Frontend\Page\PageRepository */
+		$sysPageObject = GeneralUtility::makeInstance('TYPO3\\CMS\\Frontend\\Page\\PageRepository');
+		$rootLine = $sysPageObject->getRootLine($pageUid);
 
-		$TSObj = GeneralUtility::makeInstance('t3lib_tsparser_ext');
-		$TSObj->tt_track = 0;
-		$TSObj->init();
-		$TSObj->runThroughTemplates($rootLine);
-		$TSObj->generateConfig();
+		/* @var $templateService \TYPO3\CMS\Core\TypoScript\ExtendedTemplateService */
+		$templateService = GeneralUtility::makeInstance('TYPO3\CMS\Core\TypoScript\ExtendedTemplateService');
+		$templateService->tt_track = 0;
+		$templateService->init();
+		$templateService->runThroughTemplates($rootLine);
+		$templateService->generateConfig();
 
-		return $TSObj->setup;
+		return $templateService->setup;
 	}
 }
