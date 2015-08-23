03db266deb735a25ecc814e7d98b17a12f61385e
Felix Nagel <info@felixnagel.com>
2015-08-23 12:45:26 +0200

    Extension is now considered TYPO3 7.4 compatible

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.1.1-dev',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.3.99',
+			'typo3' => '6.2.0-7.4.99',
 			'cms' => '',
 		),
 		'suggests' => array(
