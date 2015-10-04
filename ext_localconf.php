7eba251c488accf73ca80fe93fd605e1bfecf8fd
Felix Nagel <info@felixnagel.com>
2015-10-04 21:13:16 +0200

    Follow-up: Use boot function in extension config files

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,12 +2,11 @@
 if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
+// BE preview
+$TYPO3_CONF_VARS['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info']['genericgallery_pi1'][] =
+	'EXT:generic_gallery/Classes/Backend/Hooks/PageLayoutViewHook.php:TYPO3\GenericGallery\Backend\Hooks\PageLayoutViewHook->getExtensionSummary';
 
 $boot = function ($packageKey) {
-	// BE preview
-	$TYPO3_CONF_VARS['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info']['genericgallery_pi1'][] =
-		'EXT:generic_gallery/Classes/Backend/Hooks/PageLayoutViewHook.php:TYPO3\GenericGallery\Backend\Hooks\PageLayoutViewHook->getExtensionSummary';
-
 	// FE plugin
 	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
 		'TYPO3.' . $packageKey,
