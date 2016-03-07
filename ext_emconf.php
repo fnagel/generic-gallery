a1db59f2e748ccc381b1e51b9a9f39538655889e
Markus Klein <markus.klein@reelworx.at>
2016-03-07 13:08:59 +0100

    [TASK] Set a PHP version dependency
    Also remove the static version number in composer.json
    as this is not recommended and is always deduced from the
    tags of the repository.

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,8 +24,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.4.1-dev',
 	'constraints' => array(
 		'depends' => array(
+			'php' => '5.4.0-7.0.99',
 			'typo3' => '6.2.0-7.6.99',
-			'cms' => '',
 		),
 		'suggests' => array(
 			'filemetadata' => '',
