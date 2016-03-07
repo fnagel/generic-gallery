8f97a35922f92b7409977f2fa1910e0810939740
Markus Klein <markus.klein@reelworx.at>
2016-03-07 13:26:51 +0100

    [TASK] Use globals correctly and improve ext_localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,15 +1,16 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
+	die ('Access denied.');
 }
-// BE preview
-$TYPO3_CONF_VARS['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info']['genericgallery_pi1'][] =
-	'EXT:generic_gallery/Classes/Backend/Hooks/PageLayoutViewHook.php:TYPO3\GenericGallery\Backend\Hooks\PageLayoutViewHook->getExtensionSummary';
 
-$boot = function ($packageKey) {
+call_user_func(function() {
+	// BE preview
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info']['genericgallery_pi1'][] =
+		'EXT:generic_gallery/Classes/Backend/Hooks/PageLayoutViewHook.php:TYPO3\GenericGallery\Backend\Hooks\PageLayoutViewHook->getExtensionSummary';
+
 	// FE plugin
 	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-		'TYPO3.' . $packageKey,
+		'TYPO3.GenericGallery',
 		'Pi1',
 		array(
 			'GalleryCollection' => 'show',
@@ -21,7 +22,4 @@ $boot = function ($packageKey) {
 			'GalleryItem' => '',
 		)
 	);
-};
-
-$boot($_EXTKEY);
-unset($boot);
+});
