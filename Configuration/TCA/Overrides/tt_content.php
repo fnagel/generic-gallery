24023de98e70a088dd0a36b073851d3a5deecf2b
Mona Muzaffar <mona.muzaffar@gmx.de>
2018-04-24 16:11:49 +0200

    [TASK] Adjust TCA for TYPO3 8 and remove deprecated configurations

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -6,7 +6,7 @@ if (!defined('TYPO3_MODE')) {
 
 call_user_func(function ($packageKey) {
     $extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToLowerCamelCase($packageKey);
-    $pluginSignature = strtolower($extensionName).'_pi1';
+    $pluginSignature = strtolower($extensionName) . '_pi1';
     $configuration = \TYPO3\GenericGallery\Utility\EmConfiguration::getSettings();
 
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
@@ -15,12 +15,12 @@ call_user_func(function ($packageKey) {
         'LLL:EXT:generic_gallery/Resources/Private/Language/locallang_db.xlf:generic_gallery.plugin.title'
     );
 
-    $tempColumns = array(
+    $tempColumns = [
         // gallery type
-        'tx_generic_gallery_predefined' => array(
+        'tx_generic_gallery_predefined' => [
             'exclude' => 1,
             'label' => 'LLL:EXT:generic_gallery/Resources/Private/Language/locallang_db.xlf:generic_gallery_predefined',
-            'config' => array(
+            'config' => [
                 'type' => 'select',
                 'renderType' => 'selectSingle',
                 'allowNonIdValues' => 1,
@@ -28,69 +28,71 @@ call_user_func(function ($packageKey) {
                 'size' => 1,
                 'minitems' => 0,
                 'maxitems' => 1,
-            ),
-        ),
+            ],
+        ],
 
         // single items
-        'tx_generic_gallery_items' => array(
+        'tx_generic_gallery_items' => [
             'exclude' => 1,
             'label' => 'LLL:EXT:generic_gallery/Resources/Private/Language/locallang_db.xlf:generic_gallery_items',
-            'config' => array(
+            'config' => [
                 'type' => 'inline',
                 'foreign_table' => 'tx_generic_gallery_pictures',
                 'foreign_field' => 'tt_content_id',
-                'appearance' => array(
+                'appearance' => [
                     'useSortable' => 1,
                     'collapseAll' => 1,
                     'expandSingle' => 1,
-                ),
+                ],
                 'maxitems' => 1000,
                 'minitems' => 0,
-            ),
-        ),
+            ],
+        ],
 
         // file reference
-        'tx_generic_gallery_images' => array(
+        'tx_generic_gallery_images' => [
             'exclude' => 1,
             'label' => 'LLL:EXT:generic_gallery/Resources/Private/Language/locallang_db.xlf:generic_gallery_images',
             'config' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig(
                 'tx_generic_gallery_picture_single',
-                array(
+                [
                     'size' => 20,
                     'maxitems' => 2000,
                     'minitems' => 0,
                     'autoSizeMax' => 40,
-                    'foreign_types' => array(
-                        \TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => array(
-                            'showitem' => '
-									--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.basicoverlayPalette;genericGalleryImagePalette,
-									--palette--;;filePalette',
-                        ),
-                    ),
-                ),
+                    'overrideChildTca' => [
+                        'types' => [
+                            \TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => [
+                                'showitem' => '
+                                --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.basicoverlayPalette;genericGalleryImagePalette,
+                                --palette--;;filePalette',
+                            ],
+                        ],
+                    ],
+                ],
                 'jpg,gif,jpeg,png'
             ),
-        ),
+        ],
 
         // collection reference
-        'tx_generic_gallery_collection' => array(
+        'tx_generic_gallery_collection' => [
             'exclude' => 1,
             'label' => 'LLL:EXT:generic_gallery/Resources/Private/Language/locallang_db.xlf:generic_gallery_collection',
-            'config' => array(
+            'config' => [
                 'type' => 'group',
                 'internal_type' => 'db',
                 'allowed' => 'sys_file_collection',
                 'size' => 1,
                 'maxitems' => 1,
                 'minitems' => 0,
-                'wizards' => array(
-                    'suggest' => array(
+                'wizards' => [
+                    'suggest' => [
                         'type' => 'suggest',
-                    ),
-                ),
-            ),
-        ),
-    );
+                    ],
+                ],
+            ],
+        ],
+    ];
 
     if ($configuration->getUseInlineCollection()) {
         unset($tempColumns['tx_generic_gallery_collection']['config']['wizards']);
