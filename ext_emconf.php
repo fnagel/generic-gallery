d3cafae6da47d22c5bd08204ed764fb93efa8f74
Felix Nagel <info@felixnagel.com>
2018-11-10 22:24:26 +0100

    [!!!][TASK] Remove TYPO3 7.x support, Allow TYPO3 CMS 9.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'php' => '5.6.0-7.2.99',
-            'typo3' => '7.2.0-8.7.99',
+            'typo3' => '8.0.0-9.5.99',
         ),
         'suggests' => array(
             'filemetadata' => '',
