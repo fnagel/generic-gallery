4740209247eea720d71749981227539c9ea633bd
Felix Nagel <info@felixnagel.com>
2019-12-11 17:56:06 +0100

    [CLEANUP] Remove obsolete createDirs directive in EM config
    https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/10.0/Breaking-88525-RemoveCreateDirsDirectiveOfExtensionInstallationEm_confphp.html

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,7 +18,6 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'info@felixnagel.com',
     'state' => 'stable',
     'uploadfolder' => '1',
-    'createDirs' => '',
     'clearCacheOnLoad' => 1,
     'version' => '3.0.1-dev',
     'constraints' => [
