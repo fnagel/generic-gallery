b8ee2d308d72eb98a08ce41eb2cda5af7f7e6db7
Felix Nagel <info@felixnagel.com>
2019-12-18 23:57:49 +0100

    [!!!][TASK] Remove support for TYPO3 8.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.0.0-7.2.99',
-            'typo3' => '8.0.0-9.5.99',
+            'typo3' => '9.0.0-9.5.99',
         ],
         'suggests' => [
             'filemetadata' => '',
