085b36c4028bbe813b2342e04d74e422d130fde0
Felix Nagel <info@felixnagel.com>
2021-06-11 01:03:59 +0200

    [TASK] Add TYPO3 11 LTS and PHP 8.0 support, remove support for TYPO3 9.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,8 +21,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '4.2.4-dev',
     'constraints' => [
         'depends' => [
-            'php' => '7.2.0-7.4.99',
-            'typo3' => '9.4.0-10.4.99',
+            'php' => '7.2.0-8.0.99',
+            'typo3' => '10.4.0-11.5.99',
         ],
         'suggests' => [
             'filemetadata' => '',
