69a93a0189aff09931621cfa0c47913eb9e63ed9
Felix Nagel <info@felixnagel.com>
2021-11-17 18:28:11 +0100

    [TASK] Allow TYPO3 version >= 11.4 only due to use statements in localconf and exttables file
    See https://docs.typo3.org/m/typo3/reference-coreapi/master/en-us/ExtensionArchitecture/ConfigurationFiles/Index.html

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.2.0-8.0.99',
-            'typo3' => '11.0.0-11.5.99',
+            'typo3' => '11.4.0-11.5.99',
         ],
         'suggests' => [
             'filemetadata' => '',
