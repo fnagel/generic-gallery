d038b6db18166df5a9dd75a79f9d1e462c0f07ac
Felix Nagel <info@felixnagel.com>
2021-12-03 01:09:15 +0100

    [CLEANUP] Remove useless CSH files

--- ext_tables.php
+++ ext_tables.php
@@ -5,16 +5,7 @@ if (!defined('TYPO3_MODE')) {
 }
 
 call_user_func(function () {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-        'tx_generic_gallery_pictures',
-        'EXT:generic_gallery/Resources/Private/Language/locallang_csh_tx_genericgallery_domain_model_galleryitem.xlf'
-    );
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_generic_gallery_pictures');
-
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-        'tx_generic_gallery_content',
-        'EXT:generic_gallery/Resources/Private/Language/locallang_csh_tx_genericgallery_domain_model_textitem.xlf'
-    );
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_generic_gallery_content');
 
     /* @var $iconRegistry \TYPO3\CMS\Core\Imaging\IconRegistry */
