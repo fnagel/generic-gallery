10022d361be0200a108eff4f8bb66ab424b57bf8
Felix Nagel <info@felixnagel.com>
2021-12-13 23:05:57 +0100

    [TASK] Remove support for < PHP 7.4
    Makes it easier to test and TYPO3 11 only allows PHP 7.4 only anyway.
    
    See https://typo3.org/cms/roadmap

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '5.0.1-dev',
     'constraints' => [
         'depends' => [
-            'php' => '7.2.0-8.0.99',
+            'php' => '7.4.0-8.0.99',
             'typo3' => '11.4.0-11.5.99',
         ],
         'suggests' => [
