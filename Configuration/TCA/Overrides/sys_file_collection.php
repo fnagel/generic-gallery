4ea32dac10346c82c7af4f8c4d1150221b7675cc
Felix Nagel <info@felixnagel.com>
2023-11-20 16:08:08 +0100

    [TASK] Fix allowed file extension for custom image collection
    https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.0/Feature-98479-NewTCATypeFile.html

--- Configuration/TCA/Overrides/sys_file_collection.php
+++ Configuration/TCA/Overrides/sys_file_collection.php
@@ -1,7 +1,6 @@
 <?php
 
 use FelixNagel\GenericGallery\Utility\EmConfiguration;
-use TYPO3\CMS\Core\Resource\Filter\FileExtensionFilter;
 
 defined('TYPO3') || die();
 
@@ -18,27 +17,7 @@ call_user_func(static function () {
         $GLOBALS['TCA']['sys_file_collection']['types']['images']['columnsOverrides'] = [
             'files' => [
                 'config' => [
-                    'overrideChildTca' => [
-                        'columns' => [
-                            'uid_local' => [
-                                'config' => [
-                                    'appearance' => [
-                                        'elementBrowserType' => 'file',
-                                        'elementBrowserAllowed' => $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'],
-                                    ],
-                                ],
-                            ],
-                        ],
-                    ],
-                    'filter' => [
-                        [
-                            'userFunc' => FileExtensionFilter::class . '->filterInlineChildren',
-                            'parameters' => [
-                                'allowedFileExtensions' => $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'],
-                                'disallowedFileExtensions' => ''
-                            ],
-                        ],
-                    ],
+                    'allowed' => $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'],
                 ],
             ],
         ];
