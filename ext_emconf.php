7058f652629bfced5dc9c0ed6b5b2585dc72bb80
Christian Weiske <weiske@mogic.com>
2024-12-12 13:09:28 +0100

    [TASK] Declare compatible with PHP up to 8.3
    .. since TYPO3v11 itself is compatible with PHP 7.4-8.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '5.2.1-dev',
     'constraints' => [
         'depends' => [
-            'php' => '7.4.0-8.1.99',
+            'php' => '7.4.0-8.3.99',
             'typo3' => '11.4.0-11.5.99',
         ],
         'suggests' => [
