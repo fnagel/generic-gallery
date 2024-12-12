be364211055e80a97bd80392fc4daeee57598e0b
Christian Weiske <weiske@mogic.com>
2024-12-12 12:57:56 +0100

    [TASK] Align ext_emconf.php PHP version constraint with composer.json

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '6.0.3-dev',
     'constraints' => [
         'depends' => [
-            'php' => '8.1.0-8.2.99',
+            'php' => '8.1.0-8.99.99',
             'typo3' => '12.0.0-12.4.99',
         ],
         'suggests' => [
