e4c931c7e6e54c4c27616924f0f2a91299435950
Felix Nagel <info@felixnagel.com>
2025-12-02 21:05:19 +0100

    [CLEANUP] Remove obsolete hidden, start and end time fields
    https://docs.typo3.org/m/typo3/reference-tca/13.4/en-us/BestPractises/CommonFields.html#enablecolumns

--- Configuration/TCA/tx_generic_gallery_pictures.php
+++ Configuration/TCA/tx_generic_gallery_pictures.php
@@ -58,43 +58,6 @@ return [
                 'type' => 'passthrough',
             ],
         ],
-        'hidden' => [
-            'exclude' => true,
-            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.visible',
-            'config' => [
-                'type' => 'check',
-                'renderType' => 'checkboxToggle',
-                'items' => [
-                    [
-                        'label' => '',
-                        'invertStateDisplay' => true
-                    ],
-                ],
-            ]
-        ],
-        'starttime' => [
-            'exclude' => true,
-            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.starttime',
-            'config' => [
-                'type' => 'datetime',
-                'default' => 0
-            ],
-            'l10n_mode' => 'exclude',
-            'l10n_display' => 'defaultAsReadonly',
-        ],
-        'endtime' => [
-            'exclude' => true,
-            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.endtime',
-            'config' => [
-                'type' => 'datetime',
-                'default' => 0,
-                'range' => [
-                    'upper' => mktime(0, 0, 0, 1, 1, 2038),
-                ]
-            ],
-            'l10n_mode' => 'exclude',
-            'l10n_display' => 'defaultAsReadonly',
-        ],
 
         'title' => [
             'exclude' => true,
